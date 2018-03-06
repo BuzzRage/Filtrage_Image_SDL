0451469afdd80acfb82a523c7a60cce8d5e716b5
BuzzRage <marius95@live.fr>
2018-03-06 00:49:52 -0500

    Indentation + prépa pour changer verrou

--- constantes.h
+++ constantes.h
@@ -2,15 +2,15 @@
 #define CONSTANTES_H_INCLUDED
 
 // Couleurs
-#define ROUGE SDL_MapRGB(Image->format,255,0,0)
-#define VERT SDL_MapRGB(Image->format,0,255,0)
-#define BLEU SDL_MapRGB(Image->format,0,0,255)
-#define NOIR SDL_MapRGB(Image->format,0,0,0)
-#define BLANC SDL_MapRGB(Image->format,255,255,255)
-#define JAUNE SDL_MapRGB(Image->format,255,255,0)
-#define CYAN SDL_MapRGB(Image->format,0,255,255)
+#define ROUGE 	SDL_MapRGB(Image->format,255,0,0)
+#define VERT  	SDL_MapRGB(Image->format,0,255,0)
+#define BLEU  	SDL_MapRGB(Image->format,0,0,255)
+#define NOIR  	SDL_MapRGB(Image->format,0,0,0)
+#define BLANC 	SDL_MapRGB(Image->format,255,255,255)
+#define JAUNE 	SDL_MapRGB(Image->format,255,255,0)
+#define CYAN  	SDL_MapRGB(Image->format,0,255,255)
 #define MAGENTA SDL_MapRGB(Image->format,255,0,255)
-#define ORANGE SDL_MapRGB(Image->format,255,127,0)
+#define ORANGE 	SDL_MapRGB(Image->format,255,127,0)
 /*
 Uint32 couleur(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 {
@@ -19,15 +19,23 @@ Uint32 couleur(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
 */
 
 #define DELAY_KEYREPEAT 10
-#define INTERVAL 100
-#define LARGEUR_SEUIL 50
+#define INTERVAL 				100
+#define LARGEUR_SEUIL 	50
+
+// struct verrou{
+//   unsigned char binarisation:1;
+//   unsigned char quantification:1;
+//   unsigned char lux:1;
+//   unsigned char chargementOK:1;
+//   unsigned char seuil_haschanged:1;
+// };
 
 struct verrou{
-  unsigned char binarisation:1;
-  unsigned char quantification:1;
-  unsigned char lux:1;
-  unsigned char chargementOK:1;
-  unsigned char seuil_haschanged:1;
+	unsigned char binarisation;
+	unsigned char quantification:1;
+	unsigned char lux:1;
+	unsigned char chargementOK:1;
+	unsigned char seuil_haschanged:1;
 };
 
 struct Filter{
